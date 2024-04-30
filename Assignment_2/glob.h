45dbb41b3e3ac746acabc24f1fb77bd89333e3c6
JeonJaeeun <jane9518@naver.com>
2024-04-30 17:25:12 +0900

    assignment2

---
+++ Assignment_2/glob.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#define SYM_TABLE_SIZE	100
+#define HASH_TABLE_SIZE 11
+
+extern yylex();
+extern char* yytext;
+
+int sym_table[SYM_TABLE_SIZE][2];
+char str_pool[200];
+
+typedef struct HTentry* HTpointer;
+typedef struct HTentry {
+    int index;
+    HTpointer next;
+}HTentry;
+
+HTpointer HT[HASH_TABLE_SIZE];
