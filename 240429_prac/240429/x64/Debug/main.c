9eaf66ad40f5516618fa0ff35064b6e92960a2a9
tunaunnie <ygw021227@gmail.com>
2024-05-01 14:11:53 +0900

    error2 and error3

--- 240429_prac/240429/x64/Debug/main.c
+++ 240429_prac/240429/x64/Debug/main.c
@@ -63,44 +63,47 @@ int main()
    printf("---------------------------------------------------------------------------\n");
 
     while ((tn = yylex()) != TEOF) {
-        switch(tn) {
-         case LINEPROCESS: line_num++; break;
-         case TCONST: printtoken(line_num, "TCONST", yytext, -1); break;                  //main.c 갔다가 symtable.c 갔다가 가능.
-          case TELSE : printtoken(line_num, "TELSE", yytext, -1); break;
-          case TIF : printtoken(line_num, "TIF", yytext, -1); break;
-          case TINT : printtoken(line_num, "TINT", yytext, -1); break;
-          case TRETURN : printtoken(line_num, "TRETURN", yytext, -1); break;
-          case TVOID : printtoken(line_num, "TVOID", yytext, -1); break;
-          case TWHILE : printtoken(line_num, "TWHILE", yytext, -1); break;
-          case TEQUAL : printtoken(line_num, "TEQUAL", yytext, -1); break;
-          case TNOTEQU : printtoken(line_num, "TNOTEQU", yytext, -1); break;
-          case TLESS : printtoken(line_num, "TLESS", yytext, -1); break;
-          case TGREAT : printtoken(line_num, "TGREAT", yytext, -1); break;
-          case TAND : printtoken(line_num, "TAND", yytext, -1); break;
-          case TOR : printtoken(line_num, "TOR", yytext, -1); break;
-          case TINC : printtoken(line_num, "TINC", yytext, -1); break;
-          case TDEC : printtoken(line_num, "TDEC", yytext, -1); break;
-          case TADDASSIGN : printtoken(line_num, "TADDASSIGN", yytext, -1); break;
-          case TSUBASSIGN : printtoken(line_num, "TSUBASSIGN", yytext, -1); break;
-          case TMULASSIGN : printtoken(line_num, "TMULASSIGN", yytext, -1); break;
-          case TDIVASSIGN : printtoken(line_num, "TDIVASSIGN", yytext, -1); break;
-          case TMODASSIGN : printtoken(line_num, "TMODASSIGN", yytext, -1); break;
-          case TPLUS : printtoken(line_num, "TPLUS", yytext, -1); break;
-          case TMINUS : printtoken(line_num, "TMINUS", yytext, -1); break;
-          case TSTAR : printtoken(line_num, "TSTAR", yytext, -1); break;
-          case TSLASH : printtoken(line_num, "TSLASH", yytext, -1); break;
-          case TMOD : printtoken(line_num, "TMOD", yytext, -1); break;
-          case TASSIGN : printtoken(line_num, "TASSIGN", yytext, -1); break;
-          case TIDENT: {                                                                //식별자인 경우만 sym_table에 저장. 식별자 저장 후, 그거랑 별개로 매 줄마다 출력하는 printtoken 해줌
-              symtable(line_num, yytext, "TIDENT");
-              if (flag != 1)
-              {
-                  printtoken(line_num, "TIDENT", yytext, sym_table_index - 1);  break;   //already exist거나 15자 초과면 출력하면 안되니까..
-              }
-              else { flag = 0; break;  }
-          }
-          case TNUMBER: printtoken(line_num, "TNUMBER", yytext, -1); break;
-          case TERROR:  break;
+        switch (tn) {
+        case LINEPROCESS: line_num++; break;
+        case TCONST: printtoken(line_num, "TCONST", yytext, -1); break;                  //main.c 갔다가 symtable.c 갔다가 가능.
+        case TELSE: printtoken(line_num, "TELSE", yytext, -1); break;
+        case TIF: printtoken(line_num, "TIF", yytext, -1); break;
+        case TINT: printtoken(line_num, "TINT", yytext, -1); break;
+        case TRETURN: printtoken(line_num, "TRETURN", yytext, -1); break;
+        case TVOID: printtoken(line_num, "TVOID", yytext, -1); break;
+        case TWHILE: printtoken(line_num, "TWHILE", yytext, -1); break;
+        case TEQUAL: printtoken(line_num, "TEQUAL", yytext, -1); break;
+        case TNOTEQU: printtoken(line_num, "TNOTEQU", yytext, -1); break;
+        case TLESS: printtoken(line_num, "TLESS", yytext, -1); break;
+        case TGREAT: printtoken(line_num, "TGREAT", yytext, -1); break;
+        case TAND: printtoken(line_num, "TAND", yytext, -1); break;
+        case TOR: printtoken(line_num, "TOR", yytext, -1); break;
+        case TINC: printtoken(line_num, "TINC", yytext, -1); break;
+        case TDEC: printtoken(line_num, "TDEC", yytext, -1); break;
+        case TADDASSIGN: printtoken(line_num, "TADDASSIGN", yytext, -1); break;
+        case TSUBASSIGN: printtoken(line_num, "TSUBASSIGN", yytext, -1); break;
+        case TMULASSIGN: printtoken(line_num, "TMULASSIGN", yytext, -1); break;
+        case TDIVASSIGN: printtoken(line_num, "TDIVASSIGN", yytext, -1); break;
+        case TMODASSIGN: printtoken(line_num, "TMODASSIGN", yytext, -1); break;
+        case TPLUS: printtoken(line_num, "TPLUS", yytext, -1); break;
+        case TMINUS: printtoken(line_num, "TMINUS", yytext, -1); break;
+        case TSTAR: printtoken(line_num, "TSTAR", yytext, -1); break;
+        case TSLASH: printtoken(line_num, "TSLASH", yytext, -1); break;
+        case TMOD: printtoken(line_num, "TMOD", yytext, -1); break;
+        case TASSIGN: printtoken(line_num, "TASSIGN", yytext, -1); break;
+        case TIDENT: {                                                                //식별자인 경우만 sym_table에 저장. 식별자 저장 후, 그거랑 별개로 매 줄마다 출력하는 printtoken 해줌
+            symtable(line_num, yytext, "TIDENT");
+            if (flag != 1)
+            {
+                printtoken(line_num, "TIDENT", yytext, sym_table_index - 1);  break;   //already exist거나 15자 초과면 출력하면 안되니까..
+            }
+            else { flag = 0; break; }
+        }
+        case TNUMBER: printtoken(line_num, "TNUMBER", yytext, -1); break;
+        case TERROR3: printf("error - illegal digit included \n"); break;
+        case TERROR2: printf("error - start with digit \n");  break;              // 숫자로 시작하는 에러 처리
+        case TERROR:  printf("error - illegal digit included \n");  break;              // illegal 문자 에러 처리
+          
         }
     }
 
