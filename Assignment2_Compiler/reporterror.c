c3e4c848c439b8a968bd11b6399c83b1fb5a3e49
jiwon <ygw021227@gmail.com>
2024-05-02 12:42:55 +0900

    commit

---
+++ Assignment2_Compiler/reporterror.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tn.h"
+#include "glob.h"
+
+/*
+	1. 숫자로 시작하는 경우
+			ex) 123id
+	2. 허용하지 않는 문자를 포함하고 있는 경우
+		ex) l@ve
+*/
+
+char separators[] = " ,;\t\n\r\n";
+
+void reporterror1(int line_num, char* yytext) {
+
+	printf("Error at line %d: start with digit.\n", line_num);
+}
+
+void reporterror2(int line_num, char* yytext) {
+
+	printf("Error at line %d: %s is not a valid.\n", line_num, yytext);
+}
+void reporterror3(int line_num, char* yytext) {
+
+	printf("Error at line %d: %s is not a valid.\n", line_num, yytext);
+}
+void reporterror4(int line_num, char* yytext) {
+
+	printf("Error at line %d: %s is not a valid.\n", line_num, yytext);
+}
